e952f9fbcba125f61a4e9da336016a5cfddda78d
G484342024 <248434@st.takushoku-u.ac.jp>
2025-11-19 14:51:11 +0900

    L3

--- ap08L3.js
+++ ap08L3.js
@@ -105,15 +105,15 @@ export function resize() {
 }
 
 // 描画処理
-//const clock = new THREE,Clock();
-//const carPosition = new THREE.Vector3();
-//const carTarget = new THREE.Vector3();
+const clock = new THREE.Clock();
+const carPosition = new THREE.Vector3();
+const carTarget = new THREE.Vector3();
 export function render(scene, car) {
-   // const time = (clock.getElapsedTime() / 20);
-   // course.getPointAt(time % 1, carPosition);
-   // car.position.copy(carPosition);
-  //  course.getPointAt((time + 0.01) %1, carTarget);
-  //  car.lookAt(carTarget);
+    const time = (clock.getElapsedTime() / 20);
+    course.getPointAt(time % 1, carPosition);
+    car.position.copy(carPosition);
+    course.getPointAt((time + 0.01) %1, carTarget);
+    car.lookAt(carTarget);
     camera.lookAt(car.position.x, car.position.y, car.position.z);
     renderer.render(scene, camera);
 }
