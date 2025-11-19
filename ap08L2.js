2f82066fd3e86211f752bb350b0f7c5287a68e8a
G484332024 <248433@st.takushoku-u.ac.jp>
2025-11-19 15:38:12 +0900

    L2

--- ap08L2.js
+++ ap08L2.js
@@ -54,6 +54,35 @@ export function init(scene, size, id, offset, texture) {
     scene.add(plane);
 
     // ビル
+    function makeBuilding(x,z,type) {
+        const height = [2,2,7,4,5];
+        const bldgH = height[type]*5;
+        const geometry = new THREE.BoxGeometry(8, bldgH*2,8);
+        const material = new THREE.MeshLambertMaterial({map: texture});
+        const sideUvS = (type*2+1)/11;
+        const sideUvE = (type*2+2)/11;
+        const topUvS = (type*2+2)/11;
+        const topUvE = (type*2+3)/11;
+        const uvs = geometry.getAttribute("uv");
+        for(let i = 0;i<48;i+=4) {
+            if(i < 16 || i>22) {
+                uvs.array[i] = sideUvS;
+                uvs.array[i+2] = sideUvE;
+            }
+            else {
+                uvs.array[i] = topUvS;
+                uvs.array[i+2] = topUvE;
+            }
+        }
+        const bldg = new THREE.Mesh(
+            geometry,
+            material
+        )
+        bldg.position.set(x,bldgH,z);
+        scene.add(bldg);
+    }
+    makeBuilding(-80,-50,1);
+    makeBuilding(-50,-35,3);
 
     // コース(描画)
     //制御てんを補間して曲線を作る
@@ -88,6 +117,22 @@ export function init(scene, size, id, offset, texture) {
 
 // コース(自動運転用)
 export function makeCourse(scene) {
+    const courseVectors = [];
+    const parts = [L2,L3,L4,L1];
+    parts.forEach((part) => {
+        part.controlPoints.forEach((p) => {
+            courseVectors.push(
+                new THREE.Vector3(
+                    p[0] + part.origin.x,
+                    0,
+                    p[1] + part.origin.z,
+                )
+            )
+        });
+    })
+    course = new THREE.CatmullRomCurve3(
+        courseVectors, true
+    )
 }
 
 // カメラを返す
